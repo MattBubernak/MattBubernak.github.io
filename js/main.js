0905751c8632bf252a3dd6ae2cae0075d00779cd
MattBubernak <soccerfreak24@gmail.com>
2015-03-21 16:07:47 -0600

    lots of updates

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
 	// Activates wow.js
 	new WOW().init();
+
+	//Allows for smooth scrolling
+	$('a[href^="#"]').on('click',function (e) {
+		    e.preventDefault();
+
+		    var target = this.hash;
+		    var $target = $(target);
+
+		    $('html, body').stop().animate({
+		        'scrollTop': $target.offset().top
+		    }, 900, 'swing', function () {
+		        window.location.hash = target;
+		    });
+	});
 });
