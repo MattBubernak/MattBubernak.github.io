e175462c3c9b9fa8482636207d83e616e9af636c
MattBubernak <soccerfreak24@gmail.com>
2015-03-21 18:37:00 -0600

    began adding content to project page

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
 	//Allows for smooth scrolling
 	$('a[href^="#"]').on('click',function (e) {
 		    e.preventDefault();
-
 		    var target = this.hash;
 		    var $target = $(target);
 
@@ -15,4 +14,22 @@ $(document).ready(function () {
 		        window.location.hash = target;
 		    });
 	});
+
+
+	// Hide all project tabs except for default
+    var cur_tab = "discGolf";
+    $(".apiTex").hide();
+    document.getElementById(cur_tab + "H").style.color = "#b69e40";
+    $("#discGolfContent").show();
+
+    // Change visibility to clicked link.
+    $(".apiCat").click(function () {
+        document.getElementById(cur_tab + "H").style.color = 'white';
+        $("#" + cur_tab + "Content").fadeOut(300);
+        cur_tab = event.target.id.toString().substr(0, event.target.id.toString().length - 1);
+        document.getElementById(cur_tab + "H").style.color = "#b69e40";
+        $("#" + cur_tab + "Content").delay(300).fadeIn(300);
+    });
+
+
 });
