a50a2316b0dc9d74f289713e6dffaabd430e5a75
MattBubernak <soccerfreak24@gmail.com>
2015-03-21 23:11:40 -0600

    lots of udpates again

--- js/main.js
+++ js/main.js
@@ -19,16 +19,28 @@ $(document).ready(function () {
 	// Hide all project tabs except for default
     var cur_tab = "discGolf";
     $(".apiTex").hide();
-    document.getElementById(cur_tab + "H").style.color = "#b69e40";
+    document.getElementById(cur_tab + "H").style.opacity=1;
     $("#discGolfContent").show();
 
     // Change visibility to clicked link.
     $(".apiCat").click(function () {
-        document.getElementById(cur_tab + "H").style.color = 'white';
+        document.getElementById(cur_tab + "H").style.opacity = .6;
         $("#" + cur_tab + "Content").fadeOut(300);
         cur_tab = event.target.id.toString().substr(0, event.target.id.toString().length - 1);
-        document.getElementById(cur_tab + "H").style.color = "#b69e40";
+        document.getElementById(cur_tab + "H").style.opacity = 1;
         $("#" + cur_tab + "Content").delay(300).fadeIn(300);
+    });
+
+    // Handle the back to top arrow. 
+    var offset = 250;
+    var duration = 300;
+    jQuery('.back-to-top').fadeOut(0);
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
     });
 
 
